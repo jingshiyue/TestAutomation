1cd4eb7cb3e48d33b7dcdd33dc91c26f3bb0d0e7
root <173302591@qq.com>
2020-04-16 09:56:01 +0800

    +

--- apps/testcase_manage/admin.py
+++ apps/testcase_manage/admin.py
@@ -4,7 +4,7 @@ from testcase_manage.models import TestCase
 
 class TestCaseAdmin(admin.ModelAdmin):
     list_per_page = 50
-    search_fields = ('casename','url',)
+    search_fields = ('casename','api',)
     list_display = ('casename','modular_name','api','user','create_time','update_time')
     # ordering = ('create_time','update_time')
     list_filter = ('product_name','modular_name',)
